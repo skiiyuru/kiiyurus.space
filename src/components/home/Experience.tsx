e193c0935c05f3855301a9b0243d1cf1ae53c60e
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-01-28 13:07:52 +0300

    add basic scene

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -1,3 +1,9 @@
+import { Canvas } from "@react-three/fiber"
+import { useRef } from "react"
+import { type DirectionalLight, DirectionalLightHelper } from "three"
+import { OrbitControls, useHelper } from "@react-three/drei"
+import { Perf } from "r3f-perf"
+
 export default function Experience() {
   return (
     <div
@@ -6,7 +12,34 @@ export default function Experience() {
       md:row-start-4 md:row-span-2 md:h-full
       lg:row-start-1 lg:col-span-4 lg:row-span-7"
     >
-      <h1>Experience</h1>
+      <Canvas
+        style={{
+          height: "100%",
+          width: "100%",
+          overflow: "hidden",
+          backgroundColor: "transparent",
+        }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [0, 0, 10],
+        }}
+        shadows
+      >
+        <Perf />
+
+        <OrbitControls />
+
+        <ambientLight intensity={1.5} />
+
+        <directionalLight intensity={2.5} position={[2, 3, 5]} />
+
+        <mesh>
+          <torusKnotGeometry />
+          <meshPhongMaterial flatShading />
+        </mesh>
+      </Canvas>
     </div>
   )
 }
