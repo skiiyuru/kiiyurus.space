15799211f720e11cc63427fecadd6476051f3ab0
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-02-06 12:14:42 +0300

    add view transitions, update page titles, remove duplicate projects

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,9 +1,6 @@
 export const baseData = {
-  title: "Stephen Kiiyuru Portfolio Site",
-  description: `Welcome to my portfolio site!
-   I'm a Front End Engineer obsessed
-   with immersive experiences.
-  `,
+  title: "Kiiyuru's Space",
+  description: `A software developer turning pixels into immersive playgrounds`,
 }
 
 export const socialData = [
