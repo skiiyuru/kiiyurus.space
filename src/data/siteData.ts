192d7baa52e8779f3d1a4c3165389b037c8c0dd9
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-05-05 19:32:25 +0300

    fix ts error

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,5 +1,3 @@
-import type { AboutSection } from '@/lib/types'
-
 export const baseData = {
   title: 'Kiiyuru\'s Space',
   description: `A software developer turning pixels into immersive playgrounds`,
@@ -35,18 +33,3 @@ export const socialData = [
 export const RESUME_LINK
   = 'https://rzei8mczw5.ufs.sh/f/v9xa1lyXWM8uzuuIBn9N28rmSJ5BEuAkwPpiOjMXavQlyT9Z'
 export const EMAIL = 'steveknganga@gmail.com'
-
-export const about_sections: AboutSection[] = [
-  {
-    slug: 'skills',
-    title: 'Crafting Tools',
-    description: 'I design and build across the stack to create experiences that are both elegant and dependable.',
-    tools: ['TypeScript', 'Python', 'Next.js', 'Astro', 'Three.js', 'Express.js', 'GraphQL', 'PostgreSQL', 'Redis', 'Docker'],
-  },
-  {
-    slug: 'family',
-    title: 'Foundation',
-    description: `'At the heart of everything is my wife Tabitha and the life we’re shaping together
-    —anchored in love, strengthened by faith, and devoted to Christ, our salvation.`,
-  },
-]
