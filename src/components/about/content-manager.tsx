f61746ee3ddc46294e2f4c26c025db73a38fd33a
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-05-06 18:53:40 +0300

    refactor: skills section

--- src/components/about/content-manager.tsx
+++ src/components/about/content-manager.tsx
@@ -5,22 +5,43 @@ import { use } from 'react'
 import Content from './content'
 
 export default function ContentManager() {
-  const { height: h } = useThree(state => state.viewport)
+  const [{ height: h }, { height: pixelHeight, width: pixelWidth }] = useThree(state => [state.viewport, state.size])
+
   // Position content ~1/3 from the top of the viewport for optimal viewing
   const offset = h * 0.34
 
   const { sections, tools } = use(AboutContext)
 
   return (
-    <Scroll>
-      {sections.map((section, idx) => (
-        <Content
-          key={section.data.title}
-          content={section.data}
-          position={[0, (-h * idx) + offset, 0]}
-          tools={tools}
-        />
-      ))}
-    </Scroll>
+    <>
+      <Scroll>
+        {sections.map((section, idx) => (
+          <Content
+            key={section.data.title}
+            content={section.data}
+            position={[0, (-h * idx) + offset, 0]}
+          />
+        ))}
+      </Scroll>
+      <Scroll html>
+        <section
+          className="grid grid-cols-2"
+          style={{
+            width: `${pixelWidth}px`,
+            height: `${pixelHeight}px`,
+          }}
+        >
+          <div className={`
+            col-start-2 m-auto  
+            sm:grid sm:grid-cols-2 sm:gap-x-10 gap-y-2 sm:text-xl
+            md:my-auto
+            lg:grid-cols-3 lg:text-2xl
+            `}
+          >
+            {tools.filter(tool => tool.data.isFilter).map(tool => <p key={tool.id}>{tool.data.title}</p>)}
+          </div>
+        </section>
+      </Scroll>
+    </>
   )
 }
--- src/components/about/content.tsx
+++ src/components/about/content.tsx
@@ -1,19 +1,12 @@
-// eslint-disable-next-line ts/ban-ts-comment
-// @ts-nocheck
-
-import type { AboutSection, Tools } from '@/lib/types'
+import type { AboutSection } from '@/lib/types'
 import useFireworks from '@/hooks/use-fireworks'
 import { Text, useScroll } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
-import * as FlexModule from '@react-three/flex'
 import { useRef } from 'react'
 import Wambui from './wambui'
 
-const { Flex, Box } = FlexModule
-
 interface Props {
   content: AboutSection
-  tools: Tools
   position: [number, number, number]
 }
 
@@ -26,19 +19,29 @@ const titleProps = {
   color: 0xFFFCE1,
 }
 
-export default function Content({ content, position, tools }: Props) {
+export default function Content({ content, position }: Props) {
   const createRandomFirework = useFireworks()
   const scroll = useScroll()
   const [{ width }, { width: pixelWidth }] = useThree(state => [state.viewport, state.size])
 
   const lastScrollOffset = useRef(0)
   const scale = Math.min(1, width / 16)
-  const descriptionSize = Math.max(scale * 0.3, 0.027)
+  const descriptionSize = Math.max(scale * 0.3, 0.033)
   const titleSize = scale * 2
 
+  const largeScreen = pixelWidth > 1000
   const SCROLL_THRESHOLD = 0.5
   const isSkillsSection = content.slug === 'skills'
   const isFamilySection = content.slug === 'family'
+  let spacing = -0.27
+  if (!largeScreen) {
+    if (isSkillsSection) {
+      spacing = -0.15
+    }
+    else {
+      spacing = -0.18
+    }
+  }
 
   useFrame(() => {
     const scrollOffset = scroll.offset
@@ -54,29 +57,13 @@ export default function Content({ content, position, tools }: Props) {
 
   return (
     <group position={position}>
-      <group>
-        <Flex dir="column" position={[0, pixelWidth > 1000 ? 0.1 : 0.2, 0]}>
-          <Box>
-            <Text {...titleProps} fontSize={titleSize}>{content.title}</Text>
-          </Box>
-          <Box marginTop={pixelWidth > 1000 ? -0.05 : 0.02}>
-            <Text {...descriptionProps} fontSize={descriptionSize} maxWidth={pixelWidth > 1000 ? width * 0.5 : width * 0.75} textAlign="center">{content.description}</Text>
-          </Box>
-        </Flex>
-        <Flex
-          dir="row"
-          position={[0.01, pixelWidth > 1000 ? -0.25 : -0.1, 0]}
-          size={[width / 2, width / 2, 0]}
-          flexWrap="wrap"
-        >
-          {isSkillsSection && tools.filter(tool => tool.data.isFilter).map(tool => (
-            <Box key={tool.id} marginRight={0.1} marginTop={0.1} centerAnchor>
-              <Text outlineOpacity={0} {...descriptionProps} fontSize={descriptionSize}>{tool.data.title}</Text>
-            </Box>
-          ))}
-        </Flex>
-        {isFamilySection && <Wambui />}
+      <group position={[0, largeScreen ? 0.1 : 0.2, 0]}>
+        <Text {...titleProps} fontSize={titleSize}>{content.title}</Text>
+        <group position-y={spacing}>
+          <Text {...descriptionProps} fontSize={descriptionSize} maxWidth={largeScreen ? width * 0.5 : width * 0.75} textAlign="center">{content.description}</Text>
+        </group>
       </group>
+      {isFamilySection && <Wambui />}
     </group>
   )
 }
